84462299198a9d94484a92c1daa8287e31a65b53
Aleksander Palyan <freeday@apmpc.dp.ua>
2010-11-25 10:30:21 +0200

    Initialize plugin

---
+++ tasks/redmine_issue_history_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :redmine_issue_history do
+#   # Task goes here
+# end
---
+++ test/redmine_issue_history_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class RedmineIssueHistoryTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
