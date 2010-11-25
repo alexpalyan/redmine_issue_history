32e164d60fad3f88d0b9b5a6b7c217384bef7357
Aleksander Palyan <freeday@apmpc.dp.ua>
2010-11-25 11:04:59 +0200

    Fix redmine plugin

--- init.rb
+++ init.rb
@@ -1,1 +1,10 @@
-# Include hook code here
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issue_history do
+  name 'Redmine Issue History plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
-require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
+# Load the normal Rails helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+
+# Ensure that we are using the temporary fixture path
+Engines::Testing.set_fixture_path
